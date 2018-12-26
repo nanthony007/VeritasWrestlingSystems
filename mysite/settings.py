994d2be05afded93f763f2a15f319dbdb3f1ee4e
Jennifer Anthony <jennifer.anthony@rasmussen.edu>
2018-12-26 13:38:15 -0500

    eight

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,11 +79,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', 'veritas'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASS', 'postgres123'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.path.join(BASE_DIR, 'postgresql-fluffy-20323')
     }
 }
 
