c363263048cddd031aeefc588fbcdeb9da4098ba
Jennifer Anthony <jennifer.anthony@rasmussen.edu>
2018-12-26 12:54:50 -0500

    third

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mysecretkey007'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'veritas-wrestling.herokuapp.com']
 
 
 # Application definition
