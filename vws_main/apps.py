7f1a7c4952c54421214542eae8356166a57d4f19
Jennifer Anthony <jennifer.anthony@rasmussen.edu>
2018-12-26 12:49:14 -0500

    firstt

---
+++ vws_main/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VwsMainConfig(AppConfig):
+    name = 'vws_main'
---
+++ vws_main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'vws_main/home.html')
+
+def contact(request):
+    return render(request, 'vws_main/basic.html', {'content':['If you would like to contact me please email me,', 'nanthony007@gmail.com']})
