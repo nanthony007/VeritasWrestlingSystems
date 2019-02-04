cde5cf24a4b4e5a57ec77ee60b9d81b1eafa3f70
Nick <nanthony007@gmail.com>
2019-02-04 02:23:16 -0800

    yikes

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,10 +127,10 @@ DATETIME_INPUT_FORMATS = ['%m/%d/%Y']
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
 
 django_heroku.settings(locals())
