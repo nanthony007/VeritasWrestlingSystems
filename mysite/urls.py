1ceed9f02be2a89f17f9564f5f5c0552e2da83dc
Nick <nanthony007@gmail.com>
2019-06-24 19:56:35 -0400

    removed blog app and blog urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vws_main.urls')),
-    path('articles/', include('blog.urls'))
 ]
 
 if settings.DEBUG:
