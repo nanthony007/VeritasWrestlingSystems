61be119a953688137347f339ff1da864848dd0a3
nanthony007 <nanthony007@gmail.com>
2019-07-22 12:31:45 -0400

    fixed caption on ts table

--- vws_main/Ecounter.py
+++ vws_main/Ecounter.py
@@ -65,10 +65,10 @@ def effective_counter_rate(x_wrestler):
 
     counter_rate = (counter_conv / counter_att) * 100
     opp_counter_rate = (opp_counter_conv / opp_counter_att) * 100
-    # print(matches.GBc2.sum(), matches.GBa.sum())
-    # print("LONG Go Behind Rate: " + str(round((matches.GBc2.sum() / matches.GBa.sum()) * 100, 2)) + "%")
-    # print("Go Behind Rate: " + str(round(matches.GBrate.mean(), 2)) + "%")
-    # print("Go Behind Rate: " + str(round(counter_rate, 2)) + "%")
-    # print("OPP Go Behind Rate: " + str(round(opp_counter_rate, 2)) + "%")
+    print(matches.GBc2.sum(), matches.GBa.sum())
+    print("LONG Go Behind Rate: " + str(round((matches.GBc2.sum() / matches.GBa.sum()) * 100, 2)) + "%")
+    print("Go Behind Rate: " + str(round(matches.GBrate.mean(), 2)) + "%")
+    print("Go Behind Rate: " + str(round(counter_rate, 2)) + "%")
+    print("OPP Go Behind Rate: " + str(round(opp_counter_rate, 2)) + "%")
     return counter_rate, opp_counter_rate
 
