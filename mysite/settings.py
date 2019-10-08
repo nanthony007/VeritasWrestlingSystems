b749dafe89e6cf503c331401089d3fb66c99edba
Nick <nanthony007@gmail.com>
2019-10-08 17:50:52 -0400

    added favicon for browser tab, collected staticfiles for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #add whitenoise
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
