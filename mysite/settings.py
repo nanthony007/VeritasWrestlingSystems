02a9312f75ba2f682808585b469060fe8e6e7e3c
Nick <nanthony007@gmail.com>
2019-10-09 14:33:45 -0400

    Remove unwanted analysis files, add AWS-S3 bucket storage for user profile pics

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'veritasanalytics.herokuapp.com', 'veritaswrestlingsystems.com']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'django_select2',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -128,16 +129,25 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
-print(MEDIA_ROOT)
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
 
+# GMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+# AWS
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
--- users/models.py
+++ users/models.py
@@ -7,18 +7,18 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     teamname = models.CharField(max_length=250)
-    image = models.ImageField(default='profile_pics/mini.png', upload_to='profile_pics')
+    image = models.ImageField(default='mini.png', upload_to='profile_pics')
     roster = models.ManyToManyField(FS_Wrestler, related_name='roster')
 
     def __str__(self):
         return r'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save()
+    # def save(self, *args, **kwargs):
+    #     super().save()
 
-        img = Image.open(self.image)
+    #     img = Image.open(self.image)
 
-        if img.height > 200 or img.width > 200:
-            output_size = (200, 200)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 200 or img.width > 200:
+    #         output_size = (200, 200)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
