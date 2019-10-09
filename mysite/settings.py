a7ee63108b91d2d4318e23d53b4ee082d5649d91
Nick <nanthony007@gmail.com>
2019-10-08 21:39:22 -0400

    collect staticfiles, fix admin imagefilefield

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+print(MEDIA_ROOT)
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
